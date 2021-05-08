87287fdc531e1e369ddf2c4ceb58a2dd5c507a4f
Volodymyr Hannibal <v.m.hannibal@gmail.com>
2021-05-08 17:38:42 +0200

    Added Particles

--- resume-app/src/App.js
+++ resume-app/src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header'
+import Particles from 'react-particles-js';
+import particlesConfig from './particlesConfig';
 
 const App = () => {
   return (
     <>
+      <span style={{ position: 'absolute'}}>
+        <Particles height="95vh" width="100vw" params={particlesConfig} />
+      </span>
       <Header />
       <Footer />
     </>
