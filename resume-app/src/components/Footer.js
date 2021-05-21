6a45cb4d2e3f86c0a3d03a34920ec445952e8e8c
Volodymyr Hannibal <v.m.hannibal@gmail.com>
2021-05-21 17:38:54 +0200

    Added social links

--- resume-app/src/components/Footer.js
+++ resume-app/src/components/Footer.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { 
+    Container 
+} from 'react-bootstrap';
+import GitHubLogo from '../images/github.svg';
+import LinkedInLogo from '../images/linkedin.svg';
 
 export default class Footer extends React.Component {
     render() {
         return (
-            <div className="text-center m-3">
-                <h5>© 2021 All Rights Reserved</h5>
-            </div>
+            <Container className="containerFooter">
+                <a href="https://github.com/vol-han" target="_blank" rel="noreferrer" title="GitHub">
+                    <img 
+                        className="logoSocialFooter" 
+                        src={GitHubLogo} 
+                        alt="GitHub Logo"
+                    />
+                </a>
+                <a href="https://www.linkedin.com/in/vol-han/" target="_blank" rel="noreferrer" title="LinkedIn">
+                    <img 
+                        className="logoSocialFooter" 
+                        src={LinkedInLogo} 
+                        alt="LinkedIn Logo"
+                    />
+                </a>
+            </Container>
         )
     }
 }
