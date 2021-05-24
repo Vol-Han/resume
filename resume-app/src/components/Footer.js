8fa3f7513b85e508307062033ae513c1e83e3418
Volodymyr Hannibal <v.m.hannibal@gmail.com>
2021-05-24 21:52:09 +0200

    New div block in footer

--- resume-app/src/components/Footer.js
+++ resume-app/src/components/Footer.js
@@ -1,36 +1,39 @@
 import React from 'react';
 import GitHubLogo from '../images/github.svg';
 import LinkedInLogo from '../images/linkedin.svg';
+import { Container } from 'react-bootstrap';
 
 export default class Footer extends React.Component {
     render() {
         return (
-            <div className="footer">
-                <a 
-                    href="https://github.com/vol-han" 
-                    target="_blank" 
-                    rel="noreferrer" 
-                    title="GitHub"
-                >
-                    <img 
-                        className="logoSocialFooter" 
-                        src={GitHubLogo} 
-                        alt="GitHub Logo"
-                    />
-                </a>
-                <a 
-                    href="https://www.linkedin.com/in/vol-han/" 
-                    target="_blank" 
-                    rel="noreferrer" 
-                    title="LinkedIn"
-                >
-                    <img 
-                        className="logoSocialFooter" 
-                        src={LinkedInLogo} 
-                        alt="LinkedIn Logo"
-                    />
-                </a>
-            </div>
+            <Container className="footer">
+                <div>
+                    <a 
+                        href="https://github.com/vol-han" 
+                        target="_blank" 
+                        rel="noreferrer" 
+                        title="GitHub"
+                    >
+                        <img 
+                            className="logoSocialFooter" 
+                            src={GitHubLogo} 
+                            alt="GitHub Logo"
+                        />
+                    </a>
+                    <a 
+                        href="https://www.linkedin.com/in/vol-han/" 
+                        target="_blank" 
+                        rel="noreferrer" 
+                        title="LinkedIn"
+                    >
+                        <img 
+                            className="logoSocialFooter" 
+                            src={LinkedInLogo} 
+                            alt="LinkedIn Logo"
+                        />
+                    </a>
+                </div>
+            </Container>
         )
     }
 }
