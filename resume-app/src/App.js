5e72530e840a7f44625387269c85402f9f96ea89
Volodymyr Hannibal <v.m.hannibal@gmail.com>
2021-06-30 11:11:51 +0200

    Added variables .css

--- resume-app/src/App.js
+++ resume-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import './app.css';
+import './variables.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
